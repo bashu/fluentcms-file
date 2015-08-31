4f7265a57b7c6b998afc6b6480e7715dbcf63210
Basil Shubin <basil.shubin@gmail.com>
2015-08-31 11:05:31 +0600

    added ``target`` option

--- fluentcms_file/models.py
+++ fluentcms_file/models.py
@@ -19,6 +19,13 @@ class FileItem(ContentItem):
     file = models.FileField(_("file"), upload_to=appsettings.FLUENTCMS_FILE_UPLOAD_TO)
     name = models.CharField(_("name"), max_length=255, null=True, blank=True)
 
+    target = models.CharField(_("target"), blank=True, max_length=100, choices=((
+        ("", _("same window")),
+        ("_blank", _("new window")),
+        ("_parent", _("parent window")),
+        ("_top", _("topmost frame")),
+    )), default='')
+
     class Meta:
         verbose_name = _("File")
         verbose_name_plural = _("Files")
