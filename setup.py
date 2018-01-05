6d55dcd1fe83ed512b877beb3cb76508c4caf338
Basil Shubin <basil.shubin@gmail.com>
2018-01-05 18:06:28 +0700

    fixed broken setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 # When creating the sdist, make sure the django.mo file also exists:
 if 'sdist' in sys.argv or 'develop' in sys.argv:
-    os.chdir('fluentcms_link')
+    os.chdir('fluentcms_file')
     try:
         from django.core import management
         management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
